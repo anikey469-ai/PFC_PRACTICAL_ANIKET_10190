59af7f06800ab3350b9dd7a1c96d56c155743d61
anikey469-ai <anikey469@gmail.com>
2025-12-23 23:26:40 +0530

    Update alphabet.c

--- PRACTICAL 7/alphabet.c
+++ PRACTICAL 7/alphabet.c
@@ -1,20 +1,21 @@
-#include<stdio.h>
-int main(){
-    // name:-aniket kumar
-    // erp:-10190
-    int i,j;
-    char ch='A';
-    for(i=1;i<=4;i++){
-        for(j=1;j<=i;j++){
-            printf("%c ",ch);
-            ch++;
+#include <stdio.h>
+int main() {
+    //name:-aniket kumar
+    //erp:-10190
+    int n, i, j;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        for (j = 0; j < i; j++) {
+            printf("%c", 'A' + j);
         }
         printf("\n");
     }
     return 0;
-}
-// output:-
-// A 
-// B C     
-// D E F   
-// G H I J 
+// }
+// output:
+// Enter the number of rows: 4
+// A
+// AB
+// ABC
+// ABCD
